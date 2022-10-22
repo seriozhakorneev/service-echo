ac58a2933b5b5f2dd8ed0670878f101f1c419cd6
korneevsergey <seriozhakorneev@gmail.com>
2022-10-22 17:57:41 +0300

    http-echo
    Add docs.

--- internal/entrypoint/http/v1/echo.go
+++ internal/entrypoint/http/v1/echo.go
@@ -2,7 +2,6 @@ package v1
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -25,7 +24,16 @@ func newEchoRoutes(handler *gin.RouterGroup, t usecase.Echo, l logger.Interface)
 	}
 }
 
-// TODO: DOCS
+// @Summary     Reflect
+// @Description Reflects the received data, overwrites the data if overwrite rules are active
+// @ID          reflect
+// @Tags  	    echo
+// @Accept      json
+// @Produce     json
+// @Param       request body entity.JSONObject true "Set up any json object"
+// @Success     200 {object} entity.JSONObject
+// @Failure     400 {object} response
+// @Router      /echo/reflect [post]
 func (r *EchoRoutes) reflect(c *gin.Context) {
 	var data json.RawMessage
 
@@ -37,6 +45,5 @@ func (r *EchoRoutes) reflect(c *gin.Context) {
 		return
 	}
 
-	fmt.Println(string(data))
 	c.JSON(http.StatusOK, data)
 }
